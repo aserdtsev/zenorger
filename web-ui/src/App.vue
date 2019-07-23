9e74fdb37aea36e55879d72a4a178fe65c36b472
Andrey Serdtsev <serdtsev.a@prodv.net>
2019-07-23 18:01:00 +0700

    Настройка запуска сервера и web-ui

--- web-ui/src/App.vue
+++ web-ui/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <div id="header">Header</div>
+    <div id="leftSidebar">Left Sidebar</div>
+    <div id="main">Content</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
@@ -25,4 +28,26 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+div {
+  margin: 10px;
+  border: 1px solid black;
+  font-size: 20px;
+  height: 80px;
+}
+
+#header {
+  background-color: #ccc;
+}
+
+#leftSidebar {
+  background-color: #ddd;
+  float: left;
+}
+
+#main {
+  background-color: #eee;
+  height: 200px;
+  margin-left: 170px;
+}
+
 </style>
