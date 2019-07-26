756ef838a2c3d7a2bd35a20479700f8d0eb2c73f
Andrey Serdtsev <serdtsev.a@prodv.net>
2019-07-26 09:52:00 +0700

    web-ui

--- web-ui/src/App.vue
+++ web-ui/src/App.vue
@@ -1,53 +1,24 @@
 <template>
-  <div id="app">
-<!--    <img alt="Vue logo" src="./assets/logo.png">-->
-<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    <div id="header">Header</div>
-    <div id="leftSidebar">Left Sidebar</div>
-    <div id="main">Content</div>
-  </div>
+    <div id="app" class="container">
+        <!--    <img alt="Vue logo" src="./assets/logo.png">-->
+        <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+        <div class="row">
+            <div id="header" class="col-xs-12">Zenorger</div>
+        </div>
+        <div class="row">
+            <div id="nav" class="col-xs-4">Navigation</div>
+            <div id="main" class="col-xs-8">Content</div>
+        </div>
+    </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+    // import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    // HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            // HelloWorld
+        }
+    }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-div {
-  margin: 10px;
-  border: 1px solid black;
-  font-size: 20px;
-  height: 80px;
-}
-
-#header {
-  background-color: #ccc;
-}
-
-#leftSidebar {
-  background-color: #ddd;
-  float: left;
-}
-
-#main {
-  background-color: #eee;
-  height: 200px;
-  margin-left: 170px;
-}
-
-</style>
