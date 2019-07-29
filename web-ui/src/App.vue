1fcca94288d0cf9d48101b30d1547886900a5e8e
Andrey Serdtsev <serdtsev.a@prodv.net>
2019-07-29 18:00:20 +0700

    web-ui

--- web-ui/src/App.vue
+++ web-ui/src/App.vue
@@ -2,12 +2,12 @@
     <div id="app" class="container">
         <!--    <img alt="Vue logo" src="./assets/logo.png">-->
         <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-        <div class="row align-items-center">
-            <div id="header" class="col-12">Zenorger</div>
+        <div class="row text-center">
+            <div id="header" class="col-sm-12">Zenorger</div>
         </div>
-        <div class="row align-items-start">
-            <div id="nav" class="col-4">Navigation</div>
-            <div id="main" class="col-8">Content</div>
+        <div class="row">
+            <div id="nav" class="col-sm-4">Navigation</div>
+            <div id="main" class="col-sm-8">Content</div>
         </div>
     </div>
 </template>
