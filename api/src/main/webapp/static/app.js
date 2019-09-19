e9a4ca13c67a00536c22d10e95502988aee8b246
Andrey Serdtsev <serdtsev.a@prodv.net>
2019-09-19 09:46:52 +0700

    Frontend
    Реализация отображения контекстов из API.

---
+++ api/src/main/webapp/static/app.js
@@ -0,0 +1,37 @@
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            contexts: null
+        };
+    },
+    methods: {
+        showContextTasks: function(contextId) {
+            console.log("run showContextTasks: " + contextId)
+        }
+    },
+    mounted() {
+        const instance = axios.create({
+            baseURL: 'http://localhost:8080/',
+            withCredentials: true,
+            auth: {
+                username: 'andrey.serdtsev@gmail.com',
+                password: '123456'
+            },
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                'X-Organizer-Id': '59d27375-2dfe-4bec-8002-5e6514a7bce2',
+                'X-Request-Id': createUuid()
+            }
+        });
+        instance
+            .get('api/context/list')
+            .then(response => (this.contexts = response.data));
+    }
+});
+function createUuid() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    )
+}
