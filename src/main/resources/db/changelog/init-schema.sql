2af1836fe936f97454735d9a8cf82cafa06e733c
Andrey Serdtsev <serdtsev.a@prodv.net>
2020-03-19 09:31:13 +0700

    Unit tests: + embedded porstgres

--- src/main/resources/db/changelog/init-schema.sql
+++ src/main/resources/db/changelog/init-schema.sql
@@ -1,6 +1,3 @@
---liquibase formatted sql
-
---changeset Andrey Serdtsev:init-schema
 set search_path to zenorger;
 
 create table service_user (
@@ -106,18 +103,3 @@ create table comment (
     created_at timestamptz not null,
     content text not null
 );
-
-INSERT INTO zenorger.service_user (id, login, password)
-VALUES ('70f112fd-71ba-4b85-a066-833ee2988ecd', 'andrey.serdtsev@gmail.com', '$2a$10$c.F710SsYjsiNGAI1k.Ug.UEInOP9tm5c5t3hfkpHV8/ZPNLeQm46');
-
-INSERT INTO zenorger.organizer (id, user_id, name)
-VALUES ('640021fc-4093-4dd4-84f2-5792a6116cb7', '70f112fd-71ba-4b85-a066-833ee2988ecd', 'Default organizer');
-
-INSERT INTO zenorger.context (id, organizer_id, name)
-VALUES ('8d6b92ca-7a1c-47e7-ae21-173bbb06f0d7', '640021fc-4093-4dd4-84f2-5792a6116cb7', 'Car');
-INSERT INTO zenorger.context (id, organizer_id, name)
-VALUES ('3d468462-eca5-4cb0-866e-9a579a144c24', '640021fc-4093-4dd4-84f2-5792a6116cb7', 'Home');
-INSERT INTO zenorger.context (id, organizer_id, name)
-VALUES ('85e9364e-156e-4f63-aa48-2da1468c7160', '640021fc-4093-4dd4-84f2-5792a6116cb7', 'Shop');
-INSERT INTO zenorger.context (id, organizer_id, name)
-VALUES ('42f49207-2e82-4009-9aa8-0ed5c4717584', '640021fc-4093-4dd4-84f2-5792a6116cb7', 'Work');
