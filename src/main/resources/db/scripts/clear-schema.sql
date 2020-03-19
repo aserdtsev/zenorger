85d02275f0df4b70c39694294baa04f5ef568456
Andrey Serdtsev <serdtsev.a@prodv.net>
2020-03-19 09:31:39 +0700

    Unit tests: + embedded porstgres

---
+++ src/main/resources/db/scripts/clear-schema.sql
@@ -0,0 +1,15 @@
+set search_path to zenorger;
+
+drop table project_task;
+drop table task_context;
+drop table context_task;
+drop table task_tag;
+drop table comment;
+drop table task;
+drop table tag;
+drop table context;
+drop table periodicity;
+drop table organizer;
+drop table service_user;
+drop table databasechangelog;
+drop table databasechangeloglock;
